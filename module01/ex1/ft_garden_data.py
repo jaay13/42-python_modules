88cf578e945be3b162004376062cf8473a3e2614
jaay13 <contact@jason-koch.com>
2026-07-21 15:42:07 +0200

    refactor: update ft_garden_data to include type hints and format it for flake8, change height to float

--- module01/ex1/ft_garden_data.py
+++ module01/ex1/ft_garden_data.py
@@ -1,14 +1,14 @@
-
-
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: float, age: int) -> None:
         self.name = name
-        self.height = height
+        self.hght = height
         self.age = age
-    def show(self):
-            return print(f"{self.name.capitalize()}: {self.height}cm, {self.age} days old")
 
-def main():
+    def show(self) -> None:
+        print(f"{self.name.capitalize()}: {self.hght}cm, {self.age} days old")
+
+
+def main() -> None:
     rose = Plant("rose", 25, 30)
     sunflower = Plant("sunflower", 80, 45)
     cactus = Plant("cactus", 15, 120)
@@ -17,6 +17,7 @@ def main():
     rose.show()
     sunflower.show()
     cactus.show()
+
 
 if __name__ == "__main__":
     main()
