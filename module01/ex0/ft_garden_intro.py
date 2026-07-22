dc246147a9845d0f93593223dd9e31d87b393589
jaay13 <contact@jason-koch.com>
2026-07-22 17:00:42 +0200

    fix: correct typo in welcome message in ft_garden_intro.py

--- module01/ex0/ft_garden_intro.py
+++ module01/ex0/ft_garden_intro.py
@@ -2,7 +2,7 @@ def main() -> None:
     name = "Pink Tulip"
     height = 12.5
     age = 40
-    print("=== Welcome to My Garden ==")
+    print("=== Welcome to My Garden ===")
     print(f"Plant: {name}\nHeight: {height}cm\nAge: {age} days\n")
     print("=== End of Program ===")
 
