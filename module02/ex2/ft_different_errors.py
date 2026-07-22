f0c3a5f3a3d30b0e165ac64bf7dafd1ccaac9f18
jaay13 <contact@jason-koch.com>
2026-07-22 18:50:52 +0200

    refactor: simplify error handling as task suggest only using a single 'try:' to raise multiple types of errors

--- module02/ex2/ft_different_errors.py
+++ module02/ex2/ft_different_errors.py
@@ -1,41 +1,27 @@
 def garden_operations(operation_number: int) -> None:
     if operation_number == 0:
-        try:
-            int("abc")
-        except ValueError as e:
-            print(f"Caught ValueError: {e}")
+        int("abc")
     elif operation_number == 1:
-        try:
-            10 / 0
-        except ZeroDivisionError as e:
-            print(f"Caught ZeroDivisionError: {e}")
+        10 / 0
     elif operation_number == 2:
-        try:
-            open("/non/existent/file")
-        except FileNotFoundError as e:
-            print(f"Caught FileNotFoundError: {e}")
+        open("/non/existent/file")
     elif operation_number == 3:
-        try:
-            "12" + 10
-        except TypeError as e:
-            print(f"Caught: TypeError {e}")
+        "10" + 10
     else:
-        try:
-            int(10)
-        finally:
-            print("Operation completed successfully")
+        print("Operation completed successfully")
 
 
 def test_error_types() -> None:
     print("=== Garden Error Types Demo ===")
-    try:
-        for p in range(5):
-            print(f"Testing operation {p}...")
+    for p in range(5):
+        print(f"Testing operation {p}...")
+        try:
             garden_operations(p)
-            
-    finally:
-        print("\nAll error types tested successfully!")
-
+        except (ValueError, ZeroDivisionError,
+                 FileNotFoundError, TypeError) as e:
+            print(f"Caught {e.__class__.__name__}: {e}")
+    print("\nAll error types tested successfully!")
+        
 
 def main() -> None:
     test_error_types()
