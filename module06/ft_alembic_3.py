3b27eb7fab059b527c76baa94bffd558bfccd4b4
jaay13 <contact@jason-koch.com>
2026-08-04 11:24:52 +0200

    feat: add ft_alembic_3 demonstrating `from package import submodule`

---
+++ module06/ft_alembic_3.py
@@ -0,0 +1,5 @@
+from alchemy import elements
+
+print("=== Alembic 3 ===")
+print("Accessing alchemy/elements.py using 'from ... import ...' structrue")
+print(f"Testing create_earth: {elements.create_air()}")
