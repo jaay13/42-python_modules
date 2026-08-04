226b2388a160b6de3e16368f26a31ebe8b567fb4
jaay13 <contact@jason-koch.com>
2026-08-04 11:40:08 +0200

    docs: add comments explaining relative and absolute import approaches for create_air

--- module06/alchemy/__init__.py
+++ module06/alchemy/__init__.py
@@ -1,1 +1,8 @@
+# from .elements import create_air 
+# is the 'relative' approach
+# and would be the same as writing
+# from alchemy.elements import create_air 
+# which is the 'absolute' approach
+# in this example
+
 from .elements import create_air
