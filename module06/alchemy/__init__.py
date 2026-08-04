f043f21c65e98b803f028017dcd93f3b549d616b
jaay13 <contact@jason-koch.com>
2026-08-04 15:09:52 +0200

    feat: add ft_transmutation_2 accessing lead_to_gold via `import alchemy` and re-export it through alchemy/__init__.py

--- module06/alchemy/__init__.py
+++ module06/alchemy/__init__.py
@@ -8,5 +8,6 @@
 from .elements import create_air
 from .potions import healing_potion as heal
 from .potions import strength_potion
+from .transmutation import lead_to_gold
 
-__all__ = ["create_air", "heal", "strength_potion"]
+__all__ = ["create_air", "heal", "lead_to_gold", "strength_potion"]
