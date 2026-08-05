269470bb8da12e1a52d66824918373727e463574
jaay13 <contact@jason-koch.com>
2026-08-05 12:39:16 +0200

    fix: remove extra space in print statement for import structure in ft_alembic_0.py and ft_alembic_1.py

--- module06/ft_alembic_0.py
+++ module06/ft_alembic_0.py
@@ -1,5 +1,5 @@
 import elements
 
 print("=== Alembic 0 ===")
-print("Using: 'import ... ' structure to access elements.py")
+print("Using: 'import ...' structure to access elements.py")
 print(f"Testing create_fire: {elements.create_fire()}")
