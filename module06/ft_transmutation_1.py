83ff598ac086ed36188de350704a9401410f58e6
jaay13 <contact@jason-koch.com>
2026-08-05 12:26:43 +0200

    fix: correct typo in print statement in ft_transmutation_1.py

--- module06/ft_transmutation_1.py
+++ module06/ft_transmutation_1.py
@@ -1,5 +1,5 @@
 import alchemy.transmutation
 
 print("=== Transmutation 1 ===")
-print("Import transmuation module directly")
+print("Import transmutation module directly")
 print(f"Testing lead to gold: {alchemy.transmutation.lead_to_gold()}")
