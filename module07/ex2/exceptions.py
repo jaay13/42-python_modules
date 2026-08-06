81daff134c34d4e48426f559872798b5ecda0a5e
jaay13 <contact@jason-koch.com>
2026-08-06 15:18:25 +0200

    fix(exceptions): rename strategy_name param to strategy_label

--- module07/ex2/exceptions.py
+++ module07/ex2/exceptions.py
@@ -1,7 +1,7 @@
 class InvalidStrategyError(Exception):
-    def __init__(self, creature_name: str, strategy_name: str):
+    def __init__(self, creature_name: str, strategy_label: str):
         self.error_message = (
             f"Invalid Creature '{creature_name}' "
-            f"for this {strategy_name} strategy"
+            f"for this {strategy_label} strategy"
         )
         super().__init__(self.error_message)
