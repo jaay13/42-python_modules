0127ee0c500330373d16fbc35459902699b47355
jaay13 <contact@jason-koch.com>
2026-08-06 15:18:43 +0200

    fix(strategy): raise InvalidStrategyError on invalid creature check

--- module07/ex2/strategy.py
+++ module07/ex2/strategy.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 
 from ex0.creature import Creature
 
+from .exceptions import InvalidStrategyError
+
 
 class BattleStrategy(ABC):
     @abstractmethod
@@ -10,9 +12,10 @@ class BattleStrategy(ABC):
 
     @abstractmethod
     def is_valid(self, creature: Creature) -> bool:
-        """Will return a bool indicating that a Creature is suitable for strategy"""
+        """Return whether a Creature is suitable for this strategy"""
 
     def _check_valid(self, creature: Creature) -> None:
-        """Will return a bool indicating that """
+        """Raise InvalidStrategyError if the Creature isn't suitable"""
         if not self.is_valid(creature):
-            raise Exception()
+            strategy_label = type(self).__name__.removesuffix("Strategy").lower()
+            raise InvalidStrategyError(creature._name, strategy_label)
